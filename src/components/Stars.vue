078efb62278b0ffaaf7dc89094c0e29c17f25f51
Олег Капранов <arg0nny@mail.ru>
2023-08-17 17:04:47 +0700

    Icons

---
+++ src/components/Stars.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="stars">
+    <IconStar v-for="i in 5" />
+  </div>
+</template>
+
+<script setup>
+import IconStar from '@/components/icons/IconStar.vue'
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/scss/base/settings";
+
+.stars {
+  display: flex;
+  align-items: center;
+  gap: 9px;
+  color: $color-secondary;
+
+  :deep(svg) {
+    width: 18px;
+    height: 18px;
+  }
+}
+</style>
